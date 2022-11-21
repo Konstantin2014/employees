298808a0e80e51ab10d92baddc74be1d623f10ad
Konstantin2014 <87135556+Konstantin2014@users.noreply.github.com>
2022-11-21 18:39:40 +0200

    start

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,12 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Учет сотрудников компании N</h1>
+      <h2>Щбщее число сотрудников:</h2>
+      <h2>Премии получат:</h2>
+    </div>
+  );
+};
+export default AppInfo;
